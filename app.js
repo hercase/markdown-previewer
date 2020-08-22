4865ceac17b16a6d79d07348bf401e301d412a8c
Hernan Case <her.case@live.com.ar>
2020-08-22 00:04:07 -0300

    Fixed: removed split.js for performance issues.

--- app.js
+++ app.js
@@ -12,14 +12,3 @@ var preview = document.querySelector('.preview__content');
 function updatePreview () {
   preview.innerHTML = marked(markdown.innerText);
 };
-
-Split(['.markdown', '.preview'], {
-  minSize: [500, 500],
-  elementStyle: (dimension, size, gutterSize) => ({
-      'flex-basis': `calc(${size}% - ${gutterSize}px)`,
-  }),
-  gutterStyle: (dimension, gutterSize) => ({
-      'flex-basis':  `${gutterSize}px`,
-  }),
-})
-
